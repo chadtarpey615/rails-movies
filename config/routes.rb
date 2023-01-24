dfd79e4367978a8f2efcfa671f42ef0e6e242f40
Chad Tarpey <chadtarpey@gmail.com>
2023-01-23 19:49:07 -0600

    created Movie model and movies controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resources :movies
+  # get 'movies/index'
+  # get 'movies/show'
+  # get 'movies/new'
+  # get 'movies/create'
+  # get 'movies/edit'
+  # get 'movies/update'
+  # get 'movies/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
